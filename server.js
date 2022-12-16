6772d63db2abf70e320a67c563e4e7844e42cc4b
patric-borjal <patrick.borjal@mail.mcgill.ca>
2022-12-15 23:11:55 -0500

    path param version - no bonus

--- server.js
+++ server.js
@@ -25,12 +25,36 @@ app.get("/api/users", (req, res) => {
   res.json(data);
 });
 
+// This route gets a *SINGLE* user
+app.get("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  const user = data.find((user) => user.id === Number(id));
+  res.json(user);
+});
+
+// app.get("/api/users", (req, res) => {
+//   const id = req.query.id; 
+
+// });
+
 // Add a new route to get a *SINGLE* user (you can use either path param or query param)
 // /api/users/1      <-- path param (req.params.id)
 // /api/users?id=1   <-- query param (req.query.id) If you go with query param, just modify the existing endpoint above instead of creating a new endpoint
 
 // BONUS QUESTION - Add routes to implement all the CRUD operations (POST, PUT, DELETE)
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+// app.post("/", (req, res) => {
+
+// });
+
+// app.put("/", (req, res) => {
+
+// });
+
+// app.delete("/", (req, res) => {
+
+// });
+
+app.listen(3333, () => {
+  console.log("Example app listening on port 3333!");
 });
